589b7e638e480b792f4e430f86137ffa91cd2b82
Alexandru <ap.tyto@gmail.com>
2024-06-04 10:53:29 +0200

    update project - 04.06.24 - 10:53

--- src/main/java/loolu/loolu_backend/dto/UserDto.java
+++ src/main/java/loolu/loolu_backend/dto/UserDto.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import loolu.loolu_backend.models.User;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -17,4 +22,19 @@ public class UserDto {
     private String email;
     private String role;
 
+    public static UserDto from(User user){
+        return UserDto.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .role(user.getRole().toString())
+                .build();
+    }
+
+    public static List<UserDto> from(Set<User> users){
+        return users.stream()
+                .map(UserDto::from)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/loolu/loolu_backend/models/User.java
+++ src/main/java/loolu/loolu_backend/models/User.java
@@ -1,4 +1,53 @@
 package loolu.loolu_backend.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "account")
 public class User {
+
+    public enum Role{
+        ADMIN, MANAGER, USER
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 25)
+    private String firstName;
+
+    @Column(length = 25)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @ManyToMany
+    @JoinTable(
+            name = "cart_products",
+            joinColumns =
+            @JoinColumn(name = "product_id",nullable = false,referencedColumnName = "id"),
+            inverseJoinColumns =
+            @JoinColumn (name = "cart_id",nullable = false,referencedColumnName = "id"),
+            uniqueConstraints = @UniqueConstraint(columnNames = {"product_id","cart_id"})
+    )
+    private Set<Cart> carts;
 }

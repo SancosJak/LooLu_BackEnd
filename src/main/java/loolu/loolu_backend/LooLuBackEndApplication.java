8b11019973dfa802b79193a6b9a23a89b30a85c2
trashoou <dima2002zxz@gmail.com>
2024-07-01 20:53:26 +0200

    deploy v2

--- src/main/java/loolu/loolu_backend/LooLuBackEndApplication.java
+++ src/main/java/loolu/loolu_backend/LooLuBackEndApplication.java
@@ -2,8 +2,10 @@ package loolu.loolu_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "loolu.loolu_backend.repositories")
 public class LooLuBackEndApplication {
 
     public static void main(String[] args) {
